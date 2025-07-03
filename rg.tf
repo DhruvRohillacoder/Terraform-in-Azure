82439b4476f3605a55ec6278554560e3708c22cf
Dhruv <dhruvrohilla9350@gmail.com>
2025-07-03 16:43:52 +0530

    vm added

--- rg.tf
+++ rg.tf
@@ -2,3 +2,29 @@ resource "azurerm_resource_group" "name" {
   name     = "example-resources"
   location = "West Europe"
 }
+
+resource "azurerm_key_vault" "name" {
+  name                = "example-kv"
+  location            = azurerm_resource_group.name.location
+  resource_group_name = azurerm_resource_group.name.name
+  tenant_id           = data.azurerm_client_config.current.tenant_id
+  sku_name            = "standard"
+}
+
+
+resource "azurerm_virtual_machine" "name" {
+  name                  = "example-vm"
+  location              = azurerm_resource_group.name.location
+  resource_group_name   = azurerm_resource_group.name.name
+  network_interface_ids = [azurerm_network_interface.name.id]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_os_disk {
+    name              = "myosdisk1"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+}
+
+
